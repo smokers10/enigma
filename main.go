2402c6d57692ca3806e44e7f71ca74bd4b778b8c
nadzar mutaqin <nadzarmutaqin4@netmonk.id>
2023-07-12 16:53:06 +0700

    add option

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math/big"
 
 	"gopkg.in/yaml.v3"
 )
@@ -28,7 +29,6 @@ type Encryption struct {
 }
 
 func main() {
-	// Read the YAML file
 	yamlFile, err := ioutil.ReadFile("confidentials.yaml")
 	if err != nil {
 		log.Fatalf("Failed to read YAML file: %v", err)
@@ -41,16 +41,69 @@ func main() {
 		log.Fatalf("Failed to unmarshal YAML: %v", err)
 	}
 
-	encryption := Encryption{Key: []byte(config.Key)}
+	var option int
+	var aesType int
+	fmt.Print("Choose option \n1 => use key from YAML\n2 => auto generate key\n\n")
+	fmt.Print("choose your option : ")
+	fmt.Scan(&option)
 
-	for _, confidential := range config.Confidentials {
-		safeVal, err := encryption.EncryptMessage(confidential.Value)
-		if err != nil {
-			panic(err.Error())
+	if option == 1 {
+		encryption := Encryption{Key: []byte(config.Key)}
+
+		for _, confidential := range config.Confidentials {
+			safeVal, err := encryption.EncryptMessage(confidential.Value)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			fmt.Printf("Label: %s\nSafe Value: %s\n\n", confidential.Label, safeVal)
 		}
-
-		fmt.Printf("Label: %s\nSafe Value: %s\n\n", confidential.Label, safeVal)
 	}
+
+	if option == 2 {
+		var keylength int
+		fmt.Print("select AES algorithm \n1 => AES128\n2 => AES192\n3 => AES256\n\n")
+		fmt.Print("choose your AES algorithm : ")
+		fmt.Scan(&aesType)
+		switch aesType {
+		case 1:
+			keylength = 16
+			fmt.Println("select AES128")
+		case 2:
+			keylength = 24
+			fmt.Println("select AES192")
+		case 3:
+			keylength = 32
+			fmt.Println("select AES156")
+		}
+		key := generateRandomString(keylength)
+		fmt.Printf("key : %s\n\n", key)
+
+		encryption := Encryption{Key: []byte(key)}
+
+		for _, confidential := range config.Confidentials {
+			safeVal, err := encryption.EncryptMessage(confidential.Value)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			fmt.Printf("Label: %s\nSafe Value: %s\n\n", confidential.Label, safeVal)
+		}
+	}
+}
+
+func generateRandomString(length int) string {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	ret := make([]byte, length)
+	for i := 0; i < length; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return ""
+		}
+		ret[i] = letters[num.Int64()]
+	}
+
+	return string(ret)
 }
 
 func (e *Encryption) EncryptMessage(message string) (string, error) {
